8a0ff98bbfe7b137e86badbac74b6985a021f344
FrancoLSilva <francoleonelsilva.7793@gmail.com>
2022-11-05 18:30:26 -0300

    cambios de estructura (2)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Screens/App'
+import App from './Screens/Home/App'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
